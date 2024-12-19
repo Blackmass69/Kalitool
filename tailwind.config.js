268552d940585d4316f9ec5cdbadb28146b62b54
Stuart M <88146156+Blackmass69@users.noreply.github.com>
2024-12-19 23:39:35 +1000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  important: true, // Add this line to give Tailwind classes higher specificity
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
